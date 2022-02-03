e2f6eee0a3080c8019136e6ce796f44dae02b34f
Joud <66296587+cucumber-turtle@users.noreply.github.com>
2022-02-03 20:01:10 +1300

    Fixing style and adding necessary string header file.

--- wavefilehelper.hpp
+++ wavefilehelper.hpp
@@ -8,8 +8,9 @@
 
 #ifndef WAVE_FILE_HELPER
 #define WAVE_FILE_HELPER
+#include <string>
 
-namespace wavehelper {
+namespace wave_helper {
   typedef struct AudioFileData {
     char *filename;
     unsigned int sample_rate;
@@ -17,28 +18,28 @@ namespace wavehelper {
     unsigned int data_length;
   } WaveAudio;
 
-  class WavefileHelper{
-  private:
-    //Fields
-    char *chunk_id = "RIFF";
-    int file_size;
-    char *format = "WAVE";
-    char *subchunk1_id = "fmt ";
-    int subchunk1_size = 16;
-    int audio_format = 1; // PCM
-    int num_channels = 2;
-    int sample_rate;
-    int byte_rate;
-    int block_align;
-    int bits_per_sample;
-    char *subchunk2_id = "data";
-    int subchunk2_size; //size of data
-    
-  public:
-    void make_audio (WaveAudio wave);
+  class WaveFileHelper{
+    private:
+      //Fields
+      std::string chunk_id = "RIFF";
+      int file_size;
+      std::string format = "WAVE";
+      std::string subchunk1_id = "fmt ";
+      int subchunk1_size = 16;
+      int audio_format = 1; // PCM
+      int num_channels = 2;
+      int sample_rate;
+      int byte_rate;
+      int block_align;
+      int bits_per_sample;
+      std::string subchunk2_id = "data";
+      int subchunk2_size; //size of data
 
-    WaveAudio audio_reader ();
-  }
+    public:
+      void make_audio (WaveAudio wave);
+
+      WaveAudio audio_reader ();
+  };
 }
 
 #endif
